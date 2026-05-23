542a8a63009fdb33c513ea3856bad9345f63f952
prarthi <student@gmail.com>
2026-05-23 22:58:11 +0530

    day 2 pyhton added

--- day2.py
+++ day2.py
@@ -3,3 +3,24 @@ import requests
 import json
 import os
 
+response = requests.get("https://jsonplaceholder.typicode.com/posts/1")
+print(response.status_code)
+data = response.json()
+print(data["title"])
+
+founder_data = {
+    "product" = "first users"
+    "audience" = "indie founders"
+    "channels" = ["reddit","instagram","threads"]
+}
+
+with open("founder.json", "w") as f:
+    json.dump(founder_data,f,indent= 2)
+
+with open("founder.json","r") as f:
+    loaded = json.load(f)
+    print(loaded["channels"])
+
+os.environ["MY_KEY"] = "test123"
+key = os.environ.get("MY_KEY", "not found")
+print(key)
